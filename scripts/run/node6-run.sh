462aad45c17f9f9e40f6d68b1508544b98bd873a
yillkid <yillkid@gmail.com>
2017-06-07 13:39:10 +0000

    Add iri service launch scripts for puyuma nodes

--- scripts/run/node6-run.sh
+++ scripts/run/node6-run.sh
@@ -16,14 +16,13 @@ rm -f nohup.out
 rm -f hs_err_pid*.log
 
 # -XX:+UseParallelGC
-# -XX:+UseParallelGC
 nohup java -server \
         -Xmx800m -Xms128m -Xmn1g -Xss512k \
         -XX:+UseParNewGC \
         -XX:MaxMetaspaceSize=256m \
         -XX:+UseCompressedOops \
         -jar /home/ubuntu/$IRI \
-        --config iri_depoly_scripts/configs/node1-config.ini \
+        --config iri_depoly_scripts/configs/node6-config.ini \
         --remote-limit-api 'removeNeighbors, addNeighbors' --remote &
 
 sleep 2
