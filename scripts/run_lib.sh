69d53567963e518cbc7a6c9fb238e57a8b331c4d
yillkid <yillkid@gmail.com>
2017-06-24 03:10:08 +0000

    Backup old PID file when starting

--- scripts/run_lib.sh
+++ scripts/run_lib.sh
@@ -3,12 +3,20 @@
 LOG_FILE_DIR="LOGS"
 LOG_FILE_NOHUP="nohup.out"
 LOG_FILE_ROCKSDB="hs_err_pid*.log"
+LOG_FILE_IRI_PID="/var/run/iri.pid"
 
-function backup_logs ()
+function backup_logs()
 {
     # Move runtime logs to dedicated directory
     mkdir -p $LOG_FILE_DIR
     mv $LOG_FILE_ROCKSDB $LOG_FILE_DIR/
+
+    # TODO: Compression when the file size up to a specific size.
     cat $LOG_FILE_NOHUP >> $LOG_FILE_DIR/$LOG_FILE_NOHUP
+
+    # Backup PID file
+    cp $LOG_FILE_IRI_PID $LOG_FILE_DIR/
+
+    # Remove old nohup log file
     rm -f $LOG_FILE_NOHUP
 }
