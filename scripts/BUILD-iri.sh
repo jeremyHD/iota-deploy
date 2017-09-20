3d43424b0ae6cb030261abce0be0e0c4bd63a035
yillkid <yillki@gmail.com>
2017-09-20 14:29:52 +0000

    Auto install depency package

--- scripts/BUILD-iri.sh
+++ scripts/BUILD-iri.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
+DIR_IRI="iri"
+REPO_IRI="https://github.com/jserv/iri.git"
+
+# Specific IRI version to deploy
 if [ "$1" == "" ]; then
     echo "Usage: ./BUILD-iri.sh [the iri version you want to build]"
     exit 1
 fi
 
-cd iri
-# sanity checks
+# Clone IRI
+if [[ ! -e $DIR_IRI ]]; then
+    git clone $REPO_IRI 
+    sudo apt-get -y install maven
+fi
+
+# Build IRI
+cd $DIR_IRI
+
+## Sanity checks
 echo "Sanity checking ... ..."
 if [ ! -f $HOME/.m2/repository/org/rocksdb/rocksdbjni/5.8.0/rocksdbjni-5.8.0.jar ]; then
     echo "rocksdbjni-5.8.0 is not available!"
